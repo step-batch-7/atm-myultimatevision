bcb951fbc26e4ea2640d7e43e2e50697748fbe3e
nagendra <=>
2020-04-07 21:43:03 +0530

    added display_notes function to main.c to print notes count

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "atm.h"
+void display_notes(unsigned int notes);
 
 int main(void)
 {
@@ -8,7 +9,19 @@ int main(void)
   {
     unsigned int notes = get_money(amounts[i]);
     printf("%5u %08x\n", amounts[i], notes);
+    display_notes(notes);
   }
 
   return 0;
+}
+
+void display_notes(unsigned int notes){
+  int breakDown[8]={1,5,10,20,50,100,500,2000};
+  unsigned int noteCount;
+    for(int i=0;i<8;i++){
+      // printf("%u\n",notes);
+      noteCount = notes % 0x10;
+      printf("%u notes of %d\n",noteCount,breakDown[i]);
+      notes>>=4;
+    }
 }
